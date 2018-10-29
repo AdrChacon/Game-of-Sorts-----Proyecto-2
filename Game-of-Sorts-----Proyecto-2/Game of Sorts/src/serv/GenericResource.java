0131795acb3595bdfae24c30599e7f89236733d7
03-Kevin <43195779+03-Kevin@users.noreply.github.com>
2018-10-28 23:05:31 -0600

    el anterior no era

---
+++ Game-of-Sorts-----Proyecto-2/Game of Sorts/src/serv/GenericResource.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serv;
+
+import Entes.Pruebas1;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author kejor
+ */
+@Path("generic")
+public class GenericResource {
+    Pruebas1 cant = new Pruebas1();
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of game.of.sorts.imagenes.serv.GenericResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/xml")
+    public Pruebas1 getXml() {
+        
+        return cant;
+        //throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of GenericResource
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/xml")
+    public void putXml(String content) {
+    }
+}
---
+++ Game-of-Sorts-----Proyecto-2/Game of Sorts/src/serv/Serv.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serv;
+
+import Entes.Pruebas;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Kevin Rodriguez 
+ */
+@ApplicationPath ("api")
+public class Serv extends Application
+{
+
+}
