11c36267d2cfdc5300b09e838da2593413e592d5
FabianFallas <37458918+FabianFallas@users.noreply.github.com>
2018-10-28 21:54:28 -0500

    Arboles
    Se adjunta el codigo de los arboles binario, AVL y B

---
+++ Game of Sorts/src/Arboles/src/AVLNode.java
@@ -0,0 +1,23 @@
+
+    public class AVLNode {
+    public Comparable dato;      	 // el dato del nodo
+    public AVLNode izquierdo;            // hijo izquierdo
+    public AVLNode derecho;              // hijo derecho
+    public int height;                   // altura
+    public int x;
+    public int y;
+
+    // Constructors
+    public AVLNode( Comparable dato, int x, int y ){
+        this( dato, null, null, x, y );
+    }
+
+    public AVLNode( Comparable dato, AVLNode izq, AVLNode der, int x, int y ){
+        this.dato = dato;
+        this.izquierdo = izq;
+        this.derecho = der;
+        height   = 0;               // altura predeterminada
+        this.x = x;
+        this.y = y;
+    }
+}
